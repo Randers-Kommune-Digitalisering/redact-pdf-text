030e73720857031b4c0ec616233b1e0464cb3c9a
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2024-12-18 16:25:22 +0100

    Add text selection event to parent of pdfViewer

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,47 @@
 <script setup>
-    import PdfViewer from './components/PdfViewer.vue';
+    import PdfViewer from './components/PdfViewer.vue'
+    import { onMounted, ref } from 'vue'
+
+    const pdfContainer = ref(null)
+    const pdfViewer = ref(null)
+    const popup = ref(null)
+
+    const onSelectText = (text, x, y) => {
+        console.log(`Selected text: ${text}`)
+        console.log(`Coordinates: x=${x}, y=${y}`)
+    }
+
+    const showPopup = (x, y, text) => {
+        popup.value.style.left = `${x - 50}px`
+        popup.value.style.top = `${y - 50}px`
+        popup.value.textContent = text
+        popup.value.style.display = 'block'
+    }
+
+    const hidePopup = () => {
+        popup.value.style.display = 'none'
+    }
+
+    // onMounted(() => {
+    //     if (pdfContainer.value) {
+    //         pdfContainer.value.addEventListener('scroll', () => {
+    //             console.log('pdfContainer scrolled')
+    //         });
+    //     }
+    // });
 </script>
 
 <template>
-<div class="mainContainer">
-    <div class="pdfContainer"><PdfViewer source="./input.pdf" /></div>
-    <div class="optionsContainer">Test 2</div>
-</div>
+    <div class="mainContainer">
+
+        <div ref="pdfContainer" class="pdfContainer">
+            <PdfViewer :ref="pdfViewer" source="./input.pdf" @selectText="onSelectText" />
+        </div>
+        <div class="optionsContainer">Test 2</div>
+        
+    </div>
+    
+    <div ref="popup" class="popup"></div>
 </template>
 
 <style scoped>
@@ -29,5 +64,16 @@
     .optionsContainer {
         flex-grow: 1;
         background-color: #eeeeee;
+    }
+
+    .popup {
+        position: absolute;
+        display: none;
+        background: #fff;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        z-index: 1000;
+        border-radius: 0.5rem;
+        padding: 0.8rem;
     }
 </style>
